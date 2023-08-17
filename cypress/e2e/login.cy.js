aa612996e8c493329e032f241a26a2d362752929
Rafael Almeida <rafael13almeida@gmail.com>
2023-08-17 18:05:30 -0300

    Add comando customizavel de login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,6 +1,4 @@
 describe('Login', () => {
-//   const email = Cypress.env('USER_EMAIL')
-//   const senha = Cypress.env('USER_PASSWORD')
   it('login com sucesso', () => {
     cy.login()
     cy.contains('a', 'Create a new note').should('be.visible')
